c5a8632b65c39c3198312f2d191c8954907139c4
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-10-02 19:01:54 -0300

    criar helper com canais padrão

---
+++ src/helpers.js
@@ -0,0 +1,22 @@
+const defaultChannels = [
+  'fox',
+  'netgeo',
+  'discovery',
+  'bandnews',
+  'recordnews',
+  'megapix',
+  'history',
+  'hbo',
+  'telecineaction',
+  'telecinefun',
+  'telecinepremium',
+  'telecinepipoca',
+  'telecinecult',
+  'telecinetouch',
+  'fx',
+  'tnt',
+  'warner',
+  'globonews'
+]
+
+module.exports = { defaultChannels }
